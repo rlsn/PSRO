2fa26919880573b6568c09ff8e8015f68f954002
rlsn <yumo1996@gmail.com>
2024-03-29 13:55:08 +0900

    updated all

--- env.py
+++ env.py
@@ -5,101 +5,6 @@ rlsn 2024
 import numpy as np
 import gymnasium as gym ### pip install gymnasium
 from gymnasium import spaces
-from agent import Agent
-
-class Rule(object):
-    def __init__(self, n_max_energy=5, level=3, init_energy=1) -> None:
-            self.n_max_energy = n_max_energy
-            self.level = level
-            self.n_max_actions = 1 + self.level*2 # 1 yun + n attack + n defense
-            self.init_energy = init_energy
-
-            # some stats for analysis/debugging etc.
-            self.num_matches = 0 # number of matches since initialization
-            self.violation = 0 # attack without enough energy
-            self.stupidity = 0 # defense when opponent lacks energy
-            self.outpowered = 0 # attack level higher than opponent
-            self.busted = 0 # attack when opponent charges
-            self.mismatch = 0 # used wrong defense against attack
-
-    def decode_action(self, action_id):
-        if action_id is None:
-            return 0,0,0
-        if action_id == 0: # yun
-            return 1, 0, 0
-        elif action_id < self.level + 1: # attack
-            return 0, action_id, 0
-        else: # defense
-            return 0, 0 ,action_id-self.level
-
-    def encode_action(self, yun, attack, defense):
-        if yun:
-            return 0
-        elif attack:
-            return attack
-        else:
-            return defense + self.level
-
-    def available_actions(self, s1, s2):
-        A = np.zeros(self.n_max_actions)
-        A[0]=1
-        A[1:min(self.level,s1)+1]=1
-        A[self.level+1:min(self.level,s2)+self.level+1]=1
-        return A
-
-    def step(self, agent_state:int,
-             opponent_state:int,
-             agent_action:int,
-             opponent_action:int):
-        """
-        return:
-            agent_next_state (left energy)
-            opponent_next_state (left energy)
-            game_next_state (0 continue 1 opponent win, 2 agent win)
-        """
-        N = self.n_max_energy + 1 # state dimension
-        y1, a1, d1 = self.decode_action(agent_action)
-        y2, a2, d2 = self.decode_action(opponent_action)
-
-        # handle states
-        agent_next_state = min(agent_state + y1 - a1 , self.n_max_energy)
-        opponent_next_state = min(opponent_state + y2 - a2, self.n_max_energy)
-        if opponent_next_state<0:
-            # game continues with this setup
-            opponent_next_state = 0
-            a2=0 
-        game_next_state = 0
-
-        # punish invalid actions
-        if agent_next_state<0:
-            agent_next_state = 0
-            game_next_state = 1
-            self.violation+=1
-            self.num_matches+=1
-            return agent_next_state, opponent_next_state, game_next_state
-
-        # punish stupid actions
-        if d1>opponent_state:
-            game_next_state = 1
-            self.stupidity+=1
-            self.num_matches+=1
-            return agent_next_state, opponent_next_state, game_next_state
-
-        # handle result
-        if a1 and a1>a2 and a1!=d2: # agent win
-            game_next_state = 2
-        elif a2 and a2>a1 and a2!=d1: # agent loss
-            game_next_state = 1
-
-        if game_next_state>0:
-            if y1 or y2:
-                self.busted += 1
-            elif a1 and a2 and a1!=a2:
-                self.outpowered += 1
-            else:
-                self.mismatch += 1
-            self.num_matches+=1
-        return agent_next_state, opponent_next_state, game_next_state
 
 class RPSEnv(gym.Env):
     # toy env for testing sanity of the algorithm
@@ -125,11 +30,10 @@ class RPSEnv(gym.Env):
             "game_state":self._game_state
             }
 
-    def reset(self, seed=None, opponent=None, perturb=True, **kargs):
+    def reset(self, seed=None, opponent=None, **kargs):
         super().reset(seed=seed)
         if opponent is not None:
             self.opponent = opponent
-        self.perturb=perturb
         # game start
         self._game_state=0
 
@@ -167,8 +71,7 @@ class RPSEnv(gym.Env):
         else:
             self._game_state=0
             reward = 0
-        # if self.perturb:
-        #     reward+=np.random.randn() # perturbation
+
         # An episode is done iff one agent has won
         terminated = self._game_state>0
         observation = self._game_state
@@ -179,149 +82,9 @@ class RPSEnv(gym.Env):
 
         return observation, reward, terminated, truncated, info
 
-class YunEnv(gym.Env):
-    metadata = {"render_modes": ["human"], "render_fps": 4}
-
-    def __init__(self, render_mode=None, rule=None, max_episode_steps=20):
-        if rule is None:
-            rule = Rule()
-        self.rule = rule  # The rule or anything informative of the game
-        self.opponent = None
-        self.train = False
-        
-        # Observation is a Cartesian space of the agent's and the opponent's energy,
-        # as well as the current game state (ongoing 0/lose 1/win 2)
-        self.N = rule.n_max_energy + 1 
-        self.win_state_id = self.N**2+1
-        self.loss_state_id = self.N**2
-        self.n_ternimal = 2
-        self.observation_space = spaces.MultiDiscrete([self.N,self.N,3])
-        self.observation_space.n = self.N**2+2
-
-        # Action space is the maximum number of actions possible
-        self.action_space = spaces.Discrete(rule.n_max_actions)
-
-        self.reward_perturbation_std=0
-
-        assert render_mode is None or render_mode in self.metadata["render_modes"]
-        self.render_mode = render_mode
-
-        self.max_episode_steps = max_episode_steps
-
-    @staticmethod
-    def convert_obs(S1,S2,rule):
-        n = rule.n_max_energy + 1 
-        return min(S1,rule.n_max_energy) * n + min(S2,rule.n_max_energy)
-
-    @staticmethod
-    def convert_states(observation, rule):
-        n = rule.n_max_energy + 1
-        return (observation//n, observation%n)
-
-    def available_actions(self, observation):
-        if observation<self.N**2:
-            return self.rule.available_actions(*YunEnv.convert_states(observation, self.rule))
-        else:
-            return np.ones(self.action_space.n)
-
-    def _get_obs(self):
-        # agent's observation as a int
-        if self._game_state==0:
-            return self._agent_state * self.N + self._opponent_state
-        elif self._game_state==1:
-            return self.loss_state_id
-        else:
-            return self.win_state_id
-
-    def _oppo_obs(self):
-        # opponent's observation as a int
-        if self._game_state==0:
-            return self._opponent_state * self.N + self._agent_state
-        elif self._game_state==1:
-            return self.win_state_id
-        else:
-            return self.loss_state_id
-
-    def _get_info(self):
-        return {
-            "agent_action":(self._agent_action,self.rule.decode_action(self._agent_action)),
-            "opponent_action":(self._opponent_action,self.rule.decode_action(self._opponent_action)),
-            "agent_state":self._agent_state,
-            "opponent_state":self._opponent_state,
-            "observation":self._get_obs(),
-            "game_state":self._game_state
-            }
-    
-    def reset(self, seed=None, opponent=None, train=None, perturb=True):
-        # We need the following line to seed self.np_random
-        super().reset(seed=seed)
-        if train is not None:
-            self.train = train
-        if opponent is not None:
-            self.opponent = opponent
-        self.perturb=perturb
-        # game start
-        self._game_state=0
-
-        # initialize players' energy
-        if self.train:
-            self._agent_state = np.random.randint(0,self.N)
-            self._opponent_state = np.random.randint(0,self.N)
-        else:
-            self._agent_state = self.rule.init_energy
-            self._opponent_state = self.rule.init_energy
-
-        # value init
-        self._agent_action = None
-        self._opponent_action = None
-
-        observation = self._get_obs()
-        info = self._get_info()
-        self._i_step = 0
-        if self.render_mode == "human":
-            self._render_frame()
-
-        return observation, info
-    
-    def step(self, action):
-        self._agent_action = action
-        if self.opponent is not None:
-            self._opponent_action = self.opponent.step(self._oppo_obs(), Amask=self.available_actions(self._oppo_obs()))
-        else:
-            self._opponent_action = self.action_space.sample()
-        self._agent_state, self._opponent_state, self._game_state = self.rule.step(agent_state=self._agent_state,
-                                                                           opponent_state=self._opponent_state,
-                                                                           agent_action=action,
-                                                                           opponent_action=self._opponent_action)
-
-        # An episode is done iff one agent has won
-        terminated = self._game_state>0
-        # rewards
-        if self._game_state==2:
-            reward = 1
-        elif self._game_state==1:
-            reward = -1 # binary reward
-        else:
-            reward = 0
-        if self.perturb:
-            reward += np.random.randn()*self.reward_perturbation_std #perturbation
-        observation = self._get_obs()
-        info = self._get_info()
-
-        if self.render_mode == "human":
-            self._render_frame()
-
-        self._i_step+=1
-        truncated = self._i_step>=self.max_episode_steps
-
-
-        return observation, reward, terminated, truncated, info
-
 def test():
-
-    env = YunEnv()
-    # env = RPSEnv()
-
+    from agent import Agent
+    env = RPSEnv()
 
     print(env.observation_space.n)
     print(env.action_space.n)
--- train.py
+++ train.py
@@ -2,15 +2,12 @@
 Script for model training
 rlsn 2024
 """
-from env import YunEnv,RPSEnv
-from agent import Agent, tabular_Q, softmax
+from env import RPSEnv
+from agent import Agent, tabular_Q
 import numpy as np
-import argparse, random, time, itertools
+import argparse, time, itertools
 from tqdm import tqdm
 from scipy.optimize import linprog
-
-def L2norm(x):
-    return np.sqrt((x**2).sum())
 
 def solve_nash(R_matrix):
     A_ub = R_matrix
@@ -27,7 +24,7 @@ def solve_nash(R_matrix):
 def estimate_reward(env, num_episodes, p1, p2):
     R=0
     for i in range(num_episodes):
-        state, info = env.reset(opponent=p2, train=True, perturb=False)
+        state, info = env.reset(opponent=p2, train=True)
         for t in itertools.count():
             action = p1.step(state, Amask=env.available_actions(state))
             state, r, terminated, truncated, _ = env.step(action)
@@ -35,15 +32,6 @@ def estimate_reward(env, num_episodes, p1, p2):
                 R+=r
                 break
     return R/num_episodes
-
-def exploitability(env,beta,pi,Ne=300):
-    b1 = Agent(beta[0])
-    b2 = Agent(beta[1])
-    pi1 = Agent(pi[0])
-    pi2 = Agent(pi[1])
-    R1 = estimate_reward(env, Ne, b1, pi2)
-    R2 = estimate_reward(env, Ne, b2, pi1)
-    return R1, R2
 
 def exploitability_nash(env,nash_pi,pi,Ne=300):
     R = 0
@@ -62,7 +50,7 @@ def gamescape(env, pi, Ne):
             R[i,j] = estimate_reward(env,Ne,Agent(pi[i]),Agent(pi[j]))
     return R
 
-def PSROrN(env, num_iters=1000, num_steps_per_iter = 10000, eps=0.1, alpha=0.1, save_interval=1, num_policies=20):
+def PSROrN(env, num_iters=1000, num_steps_per_iter = 10000, eps=0.1, alpha=0.1, save_interval=1, num_policies=20, evaluation_episodes=500):
     nash=[]
     Pih = []
     Rh = []
@@ -71,7 +59,7 @@ def PSROrN(env, num_iters=1000, num_steps_per_iter = 10000, eps=0.1, alpha=0.1, 
     for s in range(env.observation_space.n):
         pi[:,s]*=env.available_actions(s).reshape(1,-1)
     pi = pi/pi.sum(-1,keepdims=True)
-    Ne = 500
+    Ne = evaluation_episodes
     expls = [1]
     divs = [0]
 
@@ -83,8 +71,6 @@ def PSROrN(env, num_iters=1000, num_steps_per_iter = 10000, eps=0.1, alpha=0.1, 
 
         # train agents with positive p
         for agent_id in tqdm(range(num_policies), desc="Agent training", position=1, leave=False):
-            # if nash_p[agent_id]<=0:
-            #     continue
             # reset Q
             Q = np.random.randn(env.observation_space.n,env.action_space.n)*1e-2
             Q[-env.n_ternimal:] = 0 # terminal states to 0
@@ -109,7 +95,6 @@ def PSROrN(env, num_iters=1000, num_steps_per_iter = 10000, eps=0.1, alpha=0.1, 
         nash_pi = nash_pi.sum(0)
         expl=exploitability_nash(env, nash_pi, pi, Ne=Ne)
         div = (nash_p.reshape(1,-1)@np.maximum(R,0)@nash_p.reshape(-1,1))[0,0]
-        # desc = f"eta={round(eta,4)}, expl={round(expl,4)} {nash_pi[0]}|Iter"
         desc = f"eta={round(eta,4)}, expl={round(expl,4)}, div={round(div,4)} | Iter"
         
         pbar.set_description(desc)
@@ -131,69 +116,6 @@ def PSROrN(env, num_iters=1000, num_steps_per_iter = 10000, eps=0.1, alpha=0.1, 
     }
     return data
 
-def fictitious_selfplay(env, num_iters=1000, num_steps_per_iter = 20000, eps=0.1, alpha=0.1, save_interval=5):
-    Qh, Pih = [],[]
-    pi = np.ones([2,env.observation_space.n,env.action_space.n])
-    pi = np.random.rand(2,env.observation_space.n,env.action_space.n)
-
-    pi = pi/pi.sum(-1,keepdims=True)
-
-    beta = np.copy(pi)
-    expls = [1]
-    r1,r2 = -1,-1
-    Ne = 1000
-    min_d = 0.01
-    pbar = tqdm(range(1,num_iters+1), desc="Iter", position=0)
-    for niter in pbar:        
-        # reset Q
-        Q = np.random.randn(2, env.observation_space.n,env.action_space.n)*1e-2
-        Q[:,-env.n_ternimal:] = 0 # terminal states to 0
-        # train the losing agent for beta
-        r = estimate_reward(env, Ne, Agent(pi[0]), Agent(pi[1]))
-        d = L2norm(pi[0]-pi[1])
-        if abs(r)<0.001 and d<min_d:
-            # check for policy distance (L2-norm)
-            if expl<0.01:
-                # similar policy, end training
-                print(f"early stop with exploitability={expl}")
-                break
-            else:
-                # tied, reinitialize one of them and continue
-                print(f"@ local equilibrium, reinitialize policy")
-                r_id = 1 if r1>r2 else 0
-                pi[r_id]=np.random.rand(env.observation_space.n,env.action_space.n)
-                pi[r_id] = pi[r_id]/pi[r_id].sum(-1)
-                continue
-        agent_id = 0 if r<0 else 1
-        opponent_id = agent_id-1
-
-        env.reset(opponent=Agent(pi[opponent_id]), train=True)
-        Q[agent_id] = tabular_Q(env, num_steps_per_iter, Q=Q[agent_id], epsilon=eps, alpha=alpha, eval_interval=-1)
-        beta[agent_id] = np.eye(env.action_space.n)[Q[agent_id].argmax(-1)]
-
-        # update avg strategy
-        eta = max(1/niter,0.001)
-        g_b = beta[agent_id]-pi[agent_id]
-        g_pi = pi[opponent_id]-pi[agent_id]
-        pi[agent_id] += eta*(g_b+g_pi)
-
-        # eval exploitability
-        r1,r2 = exploitability(env, beta, pi, Ne=Ne)
-        expl=r1+r2
-        # desc = f"eta={round(eta,4)}, expl={round(expl,2)} {pi[:,0].flatten()}|Iter"
-        desc = f"eta={round(eta,4)}, expl={round(expl,4)}|Iter"
-        
-        pbar.set_description(desc)
-        pbar.refresh()
-
-        # save data
-        if niter%save_interval==0:
-            Qh.append(Q)
-            Pih.append(pi)
-            expls.append(expl)
-
-    return np.array(Qh)[::-1], np.array(Pih)[::-1], expls
-
 def selfplay(env, num_iters=1000, num_steps_per_iter = 20000, eps=0.1, alpha=0.1):
     pi = np.ones([env.observation_space.n,env.action_space.n])
     pi = pi/pi.sum(-1,keepdims=True)
@@ -213,29 +135,31 @@ def selfplay(env, num_iters=1000, num_steps_per_iter = 20000, eps=0.1, alpha=0.1
         Q = tabular_Q(env, num_steps_per_iter, Q=Q, epsilon=eps, alpha=alpha, eval_interval=-1)
         beta = np.eye(env.action_space.n)[Q.argmax(-1)]
 
-
-        Ne = 1000
-        R = estimate_reward(env, Ne, Agent(beta), Agent(pi))
-        expl = R/Ne
         eta = 1/niter
         pi += eta*(beta-pi)
-
+        Ne = 1000
+        expl = estimate_reward(env, Ne, Agent(beta), Agent(pi))
 
         pbar.set_description(f"eta={round(eta,4)}, expl={round(expl,2)} |Iter")
         pbar.refresh()
-    return Q, pi
+
+
+    data = {
+        "Q":Q,
+        "pi":pi
+    }
+    return data
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', type=int, help="set seed", default=None)
     parser.add_argument('--model_file', type=str, help="filename of the model to be saved", default="Qh.npy")
-    parser.add_argument('--num_iters', type=int, help="number of total training iterations", default=20)
+    parser.add_argument('--num_iters', type=int, help="number of total training iterations", default=30)
     parser.add_argument('--num_steps_per_iter', type=int, help="number of training steps for each iteration", default=20000)
 
     parser.add_argument('--step_size', type=int, help="learning rate alpha", default=0.1)
     parser.add_argument('--eps', type=float, help="hyperparameter epsilon for epsilon greedy policy", default=0.1)
-    parser.add_argument('--num_policies', type=int, help="number of policies for PSRO", default=20)
-
+    parser.add_argument('--num_policies', type=int, help="number of policies for PSRO", default=30)
 
     args = parser.parse_args()
     
@@ -243,17 +167,14 @@ if __name__=="__main__":
         args.seed = int(time.time())
     np.random.seed(args.seed)
     print("running with seed", args.seed)
-    env = YunEnv()
-    # env = RPSEnv()
+    env = RPSEnv()
 
     print("args:",args)
 
     print("Training...")
     start = time.time()
     # Q,pi = selfplay(env, num_iters=1000, num_steps_per_iter = 20000, eps=0.1, alpha=0.01)
-    # Q,pi,expls = fictitious_selfplay(env, num_iters=args.num_iters, num_steps_per_iter = args.num_steps_per_iter, eps=args.eps, alpha=args.step_size)
     data = PSROrN(env, num_iters=args.num_iters, num_steps_per_iter = args.num_steps_per_iter, eps=args.eps, alpha=args.step_size, num_policies=args.num_policies)
-
 
     np.save(args.model_file, data)
 
